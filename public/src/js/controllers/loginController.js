54ce63530ee3136c913bbe11ece0c5a42b38dd7f
Alex Lyons <lyonsalexm@gmail.com>
2016-10-27 08:05:51 -0700

    views for login workflow

---
+++ public/src/js/controllers/loginController.js
@@ -0,0 +1,7 @@
+// public/src/js/controllers/loginController.js
+
+angular.module('loginApp', []).controller('loginCtrl', ['$scope', '$http', function($scope, $http) {
+
+  console.log("Inside login controller");
+
+}]);
